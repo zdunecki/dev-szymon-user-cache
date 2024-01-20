53b40697f49ba2f5171ae9f41a4210ee54749c27
Patryk Zdunowski <zdunekhere@gmail.com>
2024-01-20 14:30:18 +0100

    tests: add TestCacheWithoutWarm

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -50,7 +50,7 @@ func TestCache(t *testing.T) {
 				service.db[userId] = &TestUser{Id: userId}
 			}
 
-			serviceWithCache := NewCache(service)
+			serviceWithCache := NewCache[TestUser](service)
 
 			// warm the cache
 			for i := 0; i < tc.usersCount; i++ {
@@ -64,6 +64,63 @@ func TestCache(t *testing.T) {
 					t.Errorf("%s: want %s user, got %s\n", testName, userId, user.Id)
 				}
 			}
+
+			// perform concurrent requests
+			var wg sync.WaitGroup
+			for i := 0; i < tc.concurrentRequestsCount; i++ {
+				wg.Add(1)
+				userId := fmt.Sprintf("user_%d", i%tc.usersCount+1)
+
+				go func(id string) {
+					defer wg.Done()
+
+					user, err := serviceWithCache.GetOne(id)
+					if err != nil {
+						t.Errorf("%s: error getting user %s\n", testName, id)
+					}
+
+					if user.Id != id {
+						t.Errorf("%s: want %s user, got %s\n", testName, id, user.Id)
+					}
+				}(userId)
+			}
+
+			wg.Wait()
+
+			if service.dbReads != tc.usersCount {
+				t.Errorf("%s: got %d reads, want %d reads\n", testName, service.dbReads, tc.usersCount)
+			}
+		})
+	}
+}
+
+func TestCacheWithoutWarm(t *testing.T) {
+	tt := []struct {
+		concurrentRequestsCount int
+		usersCount              int
+	}{
+		{
+			concurrentRequestsCount: 1000,
+			usersCount:              100,
+		},
+		{
+			concurrentRequestsCount: 10000,
+			usersCount:              50,
+		},
+	}
+
+	for _, tc := range tt {
+		testName := fmt.Sprintf("%d users, %d requests", tc.concurrentRequestsCount, tc.usersCount)
+		t.Run(testName, func(t *testing.T) {
+			service := &mockedUserService{db: map[string]*TestUser{}}
+
+			// seeed the database
+			for i := 0; i < tc.usersCount; i++ {
+				userId := fmt.Sprintf("user_%d", i+1)
+				service.db[userId] = &TestUser{Id: userId}
+			}
+
+			serviceWithCache := NewCache[TestUser](service)
 
 			// perform concurrent requests
 			var wg sync.WaitGroup
